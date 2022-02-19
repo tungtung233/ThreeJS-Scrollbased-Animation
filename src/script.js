e93b28fc38e3cc03588d25eebe4004f6c4ae5377
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-19 17:32:52 -0500

    Add additional animated spin when user scrolls to new section

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import * as dat from 'lil-gui';
+import gsap from 'gsap';
 
 /**
  * Debug
@@ -154,9 +155,26 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Scroll
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener('scroll', () => {
   scrollY = window.scrollY;
+
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection !== currentSection) {
+    currentSection = newSection;
+
+    gsap.to(
+      sectionMeshes[currentSection].rotation,
+      {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: '+=3', // will add to the objects' current x rotation
+        y: '+=3'
+      }
+    )
+  }
 });
 
 // Cursor
@@ -192,8 +210,8 @@ const tick = () => {
 
   // Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.2;
-    mesh.rotation.y = elapsedTime * 0.2;
+    mesh.rotation.x += deltaTime * 0.2;
+    mesh.rotation.y += deltaTime * 0.2;
   }
 
   // Render
